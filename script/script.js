40cc4902058df90f09a65178b0ed3d89990918bb
kauan <kauan7.pa.krlh@gmail.com>
2025-09-05 16:20:43 -0300

    Eventos do carrosel e forms adcionados

--- script/script.js
+++ script/script.js
@@ -1,6 +1,64 @@
+//function popUp ul
 let btnProdutos = document.getElementById("produtos")
 let popUp = document.getElementById("popUp")
 
 btnProdutos.addEventListener("click" , function(){
-    popUp.classList.toggle("popUpAtivo")
+    popUp.classList.toggle("ativo")
+})
+
+// função carrosel
+
+let img1 = document.getElementById("img1");
+let img2 = document.getElementById("img2");
+let mostrando = 1; // começa na img1
+
+document.getElementById("avancaCarrosel").addEventListener("click", () => {
+    if (mostrando === 1) {
+        img1.style.display = "none";
+        img2.style.display = "block";
+        mostrando = 2;
+    }
+});
+
+document.getElementById("voltaCarrosel").addEventListener("click", () => {
+    if (mostrando === 2) {
+        img2.style.display = "none";
+        img1.style.display = "block";
+        mostrando = 1;
+    }
+});
+
+// function popUp pro form
+
+let msgEnviada = document.getElementById("MsgEnviado")
+let btnEnviar = document.getElementById("btnEnviar")
+let form = document.querySelector('form')
+let contador = 0;
+let InputNome = document.getElementById("InputNome")
+let InputSobrenome = document.getElementById("InputSobreanome")
+let InputEmail = document.getElementById("InputEmail")
+let InputDescriçao = document.getElementById("InputDescriçao")
+
+
+btnEnviar.addEventListener("click" , (e)=>{
+    e.preventDefault();
+    msgEnviada.style.display = "block"
+    
+    InputNome.value = ""
+    InputSobrenome.value = ""
+    InputEmail.value = ""
+    InputDescriçao.value = ""
+    
+    const idIntervalo = setInterval(() => {
+        msgEnviada.style.visibility = "visible"
+        
+        
+        contador++;
+        if (contador === 1) {
+            clearInterval(idIntervalo);
+        }
+    }, 900);
+    
+    msgEnviada.style.display = "none"
+    form.style.visibility = "visible"
 })
