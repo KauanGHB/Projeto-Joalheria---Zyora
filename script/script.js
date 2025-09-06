df4e7bfeab9e6c1d2ce42b7ada23c4be95b2f5e2
kauan <kauan7.pa.krlh@gmail.com>
2025-09-06 14:04:14 -0300

    form e carrosel arrumado

--- script/script.js
+++ script/script.js
@@ -1,27 +1,35 @@
 //function popUp ul
 let btnProdutos = document.getElementById("produtos")
-let popUp = document.getElementById("popUp")
+let menuDrop = document.getElementById("menuPopUp")
 
-btnProdutos.addEventListener("click" , function(){
-    popUp.classList.toggle("ativo")
+btnProdutos.addEventListener("click" , function ativamenu(){
+   alert("CHEGOU")
+   
 })
 
 // função carrosel
 
 let img1 = document.getElementById("img1");
 let img2 = document.getElementById("img2");
-let mostrando = 1; // começa na img1
+let btnCarrosel1 = document.getElementById("btnCarrosel1")
+
+let btnCarrosel2 = document.getElementById("btnCarrosel2")
+let mostrando = 1;
 
 document.getElementById("avancaCarrosel").addEventListener("click", () => {
     if (mostrando === 1) {
         img1.style.display = "none";
         img2.style.display = "block";
+        btnCarrosel2.style.color = "#ffa500"
+        btnCarrosel1.style.color = "#fff"
         mostrando = 2;
     }
 });
 
 document.getElementById("voltaCarrosel").addEventListener("click", () => {
     if (mostrando === 2) {
+        btnCarrosel1.style.color = "#ffa500"
+        btnCarrosel2.style.color = "#fff"
         img2.style.display = "none";
         img1.style.display = "block";
         mostrando = 1;
@@ -30,36 +38,32 @@ document.getElementById("voltaCarrosel").addEventListener("click", () => {
 
 // function popUp pro form
 
-let msgEnviada = document.getElementById("MsgEnviado")
-let form = document.querySelector('form')
 let InputNome = document.getElementById("InputNome")
 let InputSobrenome = document.getElementById("InputSobrenome")
 let InputEmail = document.getElementById("InputEmail")
 let InputDescricao = document.getElementById("InputDescricao")
-
+let PopUpMsg = document.getElementById("MsgEnviado")
+let form = document.querySelector('form')
 let btnEnviar = document.getElementById("btnEnviar")
-let contador = 0;
+// contadores
 
-btnEnviar.addEventListener("click" , (e)=>{
-    e.preventDefault();
-    msgEnviada.style.display = "block"
-    
-    InputNome.value = ""
-    InputSobrenome.value = ""
-    InputEmail.value = ""
-    InputDescricao.value = ""
-    
-    const idIntervalo = setInterval(() => {
-        msgEnviada.style.visibility = "visible"
-        
-        
-        contador++;
-        if (contador === 1) {
-            msgEnviada.style.visibility = "hidden"
-            clearInterval(idIntervalo);
-        }
-    }, 1000);
-
-    form.style.visibility = "visible"
-})
+btnEnviar.addEventListener("click", (e) => {
+  e.preventDefault();
+    if (
+        InputNome.value == "" ||
+        InputSobrenome.value == "" ||
+        InputEmail.value == "" ||
+        InputDescricao.value == ""
+    ) {
+        alert("há campos não preenchidos");
+    } else {
+        PopUpMsg.style.display = "block";
+        form.style.visibility = "hidden";
+        setTimeout(() => {
+            PopUpMsg.style.display = "none";
+            form.style.visibility = "visible";
+        }, 800);
+        form.reset(); // limpa campos
+    }
+});
 
